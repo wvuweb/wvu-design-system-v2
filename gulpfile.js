635ed0a28dc6d3babf50b926d06a989f1dd57e64
Adam Johnson <adam.b.johnson1@gmail.com>
2022-09-22 15:36:46 -0400

    Update almost all dependencies.
    This commit updates the following:
    
    - Bootstrap to v5.2.1
    - Moves `bootstrap.bundle.min.js` to our `javascripts/vendor` folder instead of referencing a CDN (better for performance)
    - FontFaceObserver to v2.3.0
    - Node to v16.17.0
    - Sass to their latest versions (more below)
    - Uninstalls indent.js because it doesn't appear to be used anywhere
    - Gulp-rename to v2.0.0
    
    One change of note is Sass + Gulp Sass. Sass now uses Dart instead of Node Sass. Node Sass is depreciated, but is faster than Dart Sass. I figure, moving forward, we should look towards the future. Using Dart Sass will also allow us to use newer CSS features like `:where`, `:is`, and many more.
    
    If someone doesn't like how much slower Dart Sass is, they can downgrade to Node v14 and set Node Sass as their compiler.
    
    If you don't already have this version of Node installed, you'll have to update your theme (git pull or what have you), delete your `node_modules` folder, then type `nvm install v16.17.0 && npm install -g gulp-cli && npm ci`.
    
    The only dependency that has not been "updated" is responsive-nav. I'm going to look into upgrading that to solve a Lighthouse event listener issue in a separate commit.

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,22 @@
 // Initialize modules
 // Importing specific gulp API functions lets us write
 //  them below as series() instead of gulp.series().
-var { src, dest, watch, series, parallel } = require('gulp');
-var sass = require('gulp-sass');
-var rename = require('gulp-rename');
+const { src, dest, watch, series, parallel } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const rename = require('gulp-rename');
 
 // File paths
-var files = {
+const files = {
   scssSrc: ['./scss/*.scss', '!./scss/_*.scss'],
   scssWatchPath: './scss/**/*.scss',
   scssDest: './stylesheets',
   jsDevDependenciesBase: './node_modules',
   jsDevDependenciesSrc: [
-    './node_modules/twitter-fetcher/js/twitterFetcher_min.js',
+    './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
     './node_modules/fontfaceobserver/fontfaceobserver.js',
     './node_modules/magnific-popup/dist/jquery.magnific-popup.min.js',
-    './node_modules/responsive-nav/responsive-nav.js'
+    './node_modules/responsive-nav/responsive-nav.js',
+    './node_modules/twitter-fetcher/js/twitterFetcher_min.js'
   ],
   jsDevDependenciesDest: './javascripts/vendor'
 };
@@ -26,8 +27,8 @@ var files = {
 // root `javascripts/vendor` folder.
 // It runs automatically after `npm install/ci` or can be run via
 // `npm run postinstall` OR `gulp moveJsFiles`.
-function moveJsFiles () {
-  return src(files.jsDevDependenciesSrc, { base: files.jsDevDependenciesBase })
+function moveJsFiles (done) {
+  src(files.jsDevDependenciesSrc, { base: files.jsDevDependenciesBase })
     .pipe(rename(function (file) {
     // this removes the last parent directory of the relative file path
       if (file.dirname.split('/').length > 1) {
@@ -35,22 +36,25 @@ function moveJsFiles () {
       }
     }))
     .pipe(dest(files.jsDevDependenciesDest));
+  done();
 }
 
 // Sass Task
 // Compile our Sass from the root `./scss` directory
-function sassTask () {
-  return src(files.scssSrc)
-    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+function sassTask (done) {
+  src(files.scssSrc)
+    .pipe(sass.sync({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(dest(files.scssDest));
+  done();
 }
 
 // Watch Task
 // Watches our Sass for changes
-function watchTask () {
+function watchTask (done) {
   watch(files.scssWatchPath,
     series(sassTask)
   );
+  done();
 }
 
 // Exports:
