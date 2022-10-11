aa5068eb2db53ab28bba325e6f13426230ffabe5
Adam Johnson <adam.b.johnson1@gmail.com>
2022-10-11 17:33:45 -0400

    Update responsive-nav.js.
    This update fixes:
    
    -"Use passive event listeners" error in Lighthouse
    -Remove's `aria-haspopup` from `<li> elements. #a11y
    -Uses ES6+ in the resonsive-nav-dropdown--custom.js file.
    -Gets rid of the "flash" happening to the hamburger icon on click on mobile.
    -Doesn't throw an error in the console if the mobile `.nav-toggle` button isn't found in the DOM.
    -Wraps the custom RWD nav code in an IIFE.
    
    This update also targets the wvuweb/responsive-nav.js repo. The only change to the HTML is to wrap a <span> around the Open/Close menu text, so this change should be 100% backwards compatible.

--- javascripts/responsive-nav-dropdown--custom.js
+++ javascripts/responsive-nav-dropdown--custom.js
@@ -1,27 +1,38 @@
-// Init responsive nav
-var customToggle = document.getElementById('nav-toggle');
-var navigation = responsiveNav(".nav-collapse", {
-  animate: true,                // Boolean: Use CSS3 transitions, true or false
-  insert: "before",             // String: Insert the toggle before or after the navigation
-  transition: 600,              // Integer: Speed of the transition, in milliseconds
-  customToggle: "#nav-toggle",  // Selector: Specify the ID of a custom toggle
-  enableFocus: true,            // Boolean: Do we add 'focus' class in nav elements
-  enableDropdown: true,         // Boolean: Do we use multi level dropdown
-  menuItems: "menu-items",      // String: Class that is added only to top ul element
-  subMenu: "sub-menu",
-  openPos: "relative",          // String: Class that is added to sub menu ul elements
-  openDropdown: '<span class="visually-hidden">Open sub menu</span>',    // String: Label for opening sub menu
-  closeDropdown: '<span class="visually-hidden">Close sub menu</span>',  // String: Label for closing sub menu
-  open: function () {
-    customToggle.innerHTML = '<img class="wvu-nav__menu-icon" src="https://static.wvu.edu/global/images/icons/wvu/hamburger-menu--1.0.0.svg" alt="" />Close Menu';
-  },
-  close: function () {
-    customToggle.innerHTML = '<img class="wvu-nav__menu-icon" src="https://static.wvu.edu/global/images/icons/wvu/hamburger-menu--1.0.0.svg" alt="" />Open Menu';
-  },
-  resizeMobile: function () {
-    customToggle.setAttribute( 'aria-controls', 'wvu-nav' );
-  },
-  resizeDesktop: function () {
-    customToggle.removeAttribute( 'aria-controls' );
-  },
-});
+const rwdNavDropdownCustom = (() => {
+  'use strict';
+
+  /* global responsiveNav */
+
+  const customToggle = document.querySelector('.nav-toggle');
+  if (!customToggle) return;
+
+  const customToggleText = customToggle.querySelector('#js-nav-text');
+  if (!customToggleText) return;
+
+  // Init responsive nav
+  const navigation = responsiveNav('.nav-collapse', {
+    animate: true,                // Boolean: Use CSS3 transitions, true or false
+    closeDropdown: '<span class="visually-hidden">Close sub menu</span>',  // String: Label for closing sub menu
+    customToggle: customToggle.id,  // Selector: Specify the ID of a custom toggle
+    enableDropdown: true,         // Boolean: Do we use multi level dropdown
+    enableFocus: true,            // Boolean: Do we add 'focus' class in nav elements
+    insert: 'before',             // String: Insert the toggle before or after the navigation
+    menuItems: 'menu-items',      // String: Class that is added only to top ul element
+    openDropdown: '<span class="visually-hidden">Open sub menu</span>',    // String: Label for opening sub menu
+    openPos: 'relative',          // String: Class that is added to sub menu ul elements
+    subMenu: 'sub-menu',
+    transition: 600,              // Integer: Speed of the transition, in milliseconds
+    close () {
+      customToggleText.innerText = 'Open Menu';
+    },
+    open () {
+      customToggleText.innerText = 'Close Menu';
+    },
+    resizeDesktop () {
+      customToggle.removeAttribute('aria-controls');
+    },
+    resizeMobile () {
+      customToggle.setAttribute('aria-controls', 'wvu-nav');
+    }
+  });
+})();
