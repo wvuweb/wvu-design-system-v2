bca52cdbc08bc923f45c171879f00604daeea3d7
atglenn <adam.glenn@gmail.com>
2023-10-11 12:44:51 -0400

    Adding map data template.

--- javascripts/google-map.js
+++ javascripts/google-map.js
@@ -68,7 +68,7 @@ function mapInit(){
   map.controls[google.maps.ControlPosition.TOP].push(homeControlDiv);
 	// end Center Map
 
-	$.getJSON("/beckley-map-12.json", {}, function(data){
+	$.getJSON("https://designsystemv2demo.volutus.wvu.edu/map-data.json", {}, function(data){
 			$.each(data.places, function(i, item){
 			$("#markers").append('<li><a href="#" rel="' + i + '">' + item.title + '</a></li>');
 			var marker = new google.maps.Marker({
